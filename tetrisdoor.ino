be89a9811c5e432a21575382e31b7e9ea1612acd
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2015-07-19 12:36:43 +0200

    Backup

--- tetrisdoor.ino
+++ tetrisdoor.ino
@@ -1,11 +1,26 @@
+// Based on code from Tuertris hacked by mclien
+// https://hackaday.io/project/3708-trtris
+// http://mclien.de/files/tuertris/tuertris_sw.zip
+// Joel Lienhard  (joel@mclien.de  joel@sisam42.de)
+
+
 #include "application.h"
 #include "neopixel__spark_internet_button/neopixel__spark_internet_button.h"
 
 #define PIXEL_PIN      D0
+#define MODE_PIN       D1
+#define LEFT           D2
+#define RIGHT          D3
+#define DOWN           D4
+#define ROTATE         D5
+
+#define MAX_MODE        2
+
 #define PIXEL_TYPE     WS2811
 #define matrixx         5
 #define matrixy        10
 #define fallspeedorg  100
+#define debouncevalue 100
 
 int pos[2]                   = { matrixx-3, matrixy};
 int bricks[7][4][2]          = { {{ 0,-1}, {-1, 0}, {0, 0}, {1, 0}},  // 0
@@ -27,7 +42,8 @@ int moveable                 = 0;
 int fullrow                  = 0;
 int rowtodelete              = 0;
 int gameover                 = 0;
-int mode                     = 1;
+int mode                     = 0;
+int debounce                 = 0;
 int currentColor[3]          = {0};
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel( (matrixx * matrixy) , PIXEL_PIN, PIXEL_TYPE);
@@ -36,6 +52,17 @@ void setup()
 {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  
+  pinMode(ROTATE,       INPUT);
+  pinMode(DOWN,         INPUT);
+  pinMode(LEFT,         INPUT);
+  pinMode(RIGHT,        INPUT);
+  pinMode(MODE_PIN,     INPUT);
+  digitalWrite(ROTATE,  HIGH);
+  digitalWrite(DOWN,    HIGH);
+  digitalWrite(LEFT,    HIGH);
+  digitalWrite(RIGHT,   HIGH);
+  digitalWrite(MODE_PIN, HIGH);
 }
 
 void checkend(){
@@ -276,7 +303,7 @@ void add(int b){
 
 }
 
-void show(){
+void show(bool tetris){
 
   for(int i = 0; i < strip.numPixels(); i++){
     strip.setPixelColor(i, strip.Color(0, 0, 0));
@@ -300,17 +327,20 @@ void show(){
   
   }
 
+  if (tetris) {
   
-  for(int e = 0; e < 4; e++){
-    truepos[0] = current[e][0]+pos[0];
-    truepos[1] = current[e][1]+pos[1];
+      for(int e = 0; e < 4; e++){
+        truepos[0] = current[e][0]+pos[0];
+        truepos[1] = current[e][1]+pos[1];
+      
+        if(truepos[1]%2 == 0){
+          strip.setPixelColor((truepos[1]*matrixx)+truepos[0],             strip.Color(colors[currentnum][0], colors[currentnum][1], colors[currentnum][2]) );
+        }else{
+          strip.setPixelColor((truepos[1]*matrixx)+(matrixx-truepos[0]-1), strip.Color(colors[currentnum][0], colors[currentnum][1], colors[currentnum][2]) );
+        }
+        
+      }
   
-    if(truepos[1]%2 == 0){
-      strip.setPixelColor((truepos[1]*matrixx)+truepos[0],             strip.Color(colors[currentnum][0], colors[currentnum][1], colors[currentnum][2]) );
-    }else{
-      strip.setPixelColor((truepos[1]*matrixx)+(matrixx-truepos[0]-1), strip.Color(colors[currentnum][0], colors[currentnum][1], colors[currentnum][2]) );
-    }
-    
   }
   
   strip.show();
@@ -318,6 +348,15 @@ void show(){
 
 void loop() {
     
+    if(digitalRead(MODE_PIN) == LOW and debounce == 0){
+      debounce = debouncevalue;
+      mode++;
+      if (mode > MAX_MODE){
+          mode = 0;
+      }
+    }
+
+    // RANDOM SPOCK Pattern
     if(mode == 0){
       for(int x = 0; x < matrixx; x++){
         for(int y = 0; y < matrixy; y ++){
@@ -325,23 +364,51 @@ void loop() {
         }  
       }
       
-      
       matrix[4][2] = 1;
-      show();
+      show(false);
     }
     
-    
-    
+    // Tetris
     if(mode == 1){
         if(fallcount == fallspeed){
             fallcount = 0;
             down();
         }
         
+        if(digitalRead(DOWN) == LOW and debounce == 0){
+          debounce = debouncevalue;
+          down();
+        }
+
+        if(digitalRead(RIGHT) == LOW and debounce == 0){
+          debounce = debouncevalue;
+          right();
+        }
+        
+        if(digitalRead(LEFT) == LOW and debounce == 0){
+          debounce = debouncevalue;
+          left();
+        }
+        
+        if(digitalRead(ROTATE) == LOW and debounce == 0){
+          debounce = debouncevalue; 
+          rotate();
+        }
+       
         fallcount++;
         checkrows();
         checkend();
-        show();
+        show(true);
         delay(1);
     }
+    
+    // Game of life
+    if(mode == 2){
+        
+    }
+    
+    if(debounce != 0){
+      debounce--;
+    }
+
 }
